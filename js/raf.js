491bdd9be108320ad2268903363b8b87f19599c7
cranberrymuffin <aparnanatarajan@aparna.myfiosgateway.com>
2020-11-08 01:11:53 -0500

    live sonification code (modification of https://github.com/ojack/PIXELSYNTH; added interactivity and simplified ui. code quality improvements needed.

---
+++ js/raf.js
@@ -0,0 +1,14 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
